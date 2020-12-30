088fbccc413e83e47d0da9fda04423dad1668902
Athfan Khaleel <athphane@gmail.com>
2020-12-30 15:08:05 +0500

    - create github actions webhook trigger

---
+++ main.py
@@ -0,0 +1,33 @@
+from configparser import ConfigParser
+
+import requests
+from fastapi import FastAPI
+
+# Environment Variables
+config_file = "env.ini"
+config = ConfigParser()
+config.read(config_file)
+
+secret_key = config.get('secrets', 'key')
+GITHUB_TOKEN = config.get('secrets', 'github_token')
+owner = config.get('secrets', 'owner')
+repository = config.get('secrets', 'repository')
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root(event_type: str, secret: str):
+    if secret == secret_key:
+        headers = {
+            'Accept': 'application/vnd.github.everest-preview+json',
+            'Authorization': f'token {GITHUB_TOKEN}'
+        }
+
+        url = f'https://api.github.com/repos/{owner}/{repository}/dispatches'
+
+        data = '{"event_type":"' + event_type + '"}'
+
+        res = requests.post(url, headers=headers, data=data)
+
+        return {'status': res.status_code}
